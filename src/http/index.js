90c0a2b12f0bbde9f59f0243d26ed46c51dd288f
maksoon-eu <baranovmaks05@gmail.com>
2024-01-26 11:23:04 +0300

    https

--- src/http/index.js
+++ src/http/index.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const $host = axios.create({
-    baseURL: 'http://localhost:4000/'
+    baseURL: 'https://sharyotbrusa.ru:4000/'
 })
 
 const $authHost = axios.create({
-    baseURL: 'http://localhost:4000/'
+    baseURL: 'https://sharyotbrusa.ru:4000/'
 })
 
 const authInterceptor = config => {
